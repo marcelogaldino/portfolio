62f2275b694b7192b7b4c4f241efa6fa56a68a47
marcelogaldino <marcelinhomgo@gmail.com>
2019-12-09 23:28:16 -0300

    using nunjucks for dynamic datas, and created a new route ./articles

--- public/script.js
+++ public/script.js
@@ -1,12 +1,11 @@
 const modalOverlay = document.querySelector(".modal-overlay")
-const cards = document.querySelectorAll(".card")
+const cards = document.querySelectorAll(".cards")
 
 for (let card of cards) {
     card.addEventListener("click", function() {
-        modalOverlay.classList.add("active")
+        const articleId = card.getAttribute("id")
+        window.location.href = `/article?id=${articleId}`
+        console.log(articleId)
     })
 }
 
-document.querySelector(".close-modal").addEventListener("click", function() {
-    modalOverlay.classList.remove("active")
-})
--- server.js
+++ server.js
@@ -1,22 +1,52 @@
 const express = require("express")
 const nunjucks = require("nunjucks")
 
+const articles = require("./data")
+
 const server = express()
 
 server.use(express.static("public"))
 
-server.set("view engine", "html")
+server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get("/", function(req, res) {
-    res.render("about")
+    const about = {
+        avatar_url: "https://avatars3.githubusercontent.com/u/13842365?s=460&v=4",
+        name: "Marcelo Galdino",
+        role: "Desenvolvedor web full stack",
+        description: 'Desenvolvedor web focado em aprender novas tecnologias e se tornar um full stack. Stack escolhida -> HTML, CSS, Javascript, React, React Native, Node.js. Acesse o meu <a href="https://github.com/marcelogaldino" target="_blank">Github</a>',
+        links: [
+            {url: "https://github.com/marcelogaldino/", name: "Github"},
+            {url: "https://twitter.com/oficialmarcelog/", name: "Twitter"},
+            {url: "https://www.linkedin.com/in/marcelogaldino/", name: "Linkedin"}
+        ]
+    }
+
+    res.render("about", { about })
 })
 
 server.get("/portfolio", function(req, res) {
-    res.render("portfolio")
+    res.render("portfolio", { items: articles })
+})
+
+server.get("/article", function(req, res) {
+    const id = req.query.id
+
+    const article = articles.find(function(article) {
+        return article.link == id
+
+    })
+
+    if (!article) {
+        return res.send("Article not found!")
+    }
+    
+    res.render("article", { item: article })
 })
 
 server.listen(5000, function() {
