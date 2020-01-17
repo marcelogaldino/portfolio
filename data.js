7935b6e3c8e2849592ad275869323d60d2aa79a6
marcelogaldino <marcelinhomgo@gmail.com>
2020-01-17 17:26:53 -0300

    changed articles page, new link to read the article outside the website

--- data.js
+++ data.js
@@ -5,40 +5,6 @@ module.exports = [
         duration: "5min read",
         price: "FREE",
         featured: true
-    },
-
-    {
-        link: "https://miro.medium.com/max/2560/0*nwlsfa0hEsSQihqw",
-        title: "Post sobre Javascript no medium | Afinal o que é Javascript? - Marcelo Galdino",
-        duration: "5min read",
-        price: "FREE"
-    },
-
-    {
-        link: "https://miro.medium.com/max/2560/0*nwlsfa0hEsSQihqw",
-        title: "Post sobre Javascript no medium | Afinal o que é Javascript? - Marcelo Galdino",
-        duration: "5min read",
-        price: "FREE"
-    },
-
-    {
-        link: "https://miro.medium.com/max/2560/0*nwlsfa0hEsSQihqw",
-        title: "Post sobre Javascript no medium | Afinal o que é Javascript? - Marcelo Galdino",
-        duration: "5min read",
-        price: "FREE"
-    },
-
-    {
-        link: "https://miro.medium.com/max/2560/0*nwlsfa0hEsSQihqw",
-        title: "Post sobre Javascript no medium | Afinal o que é Javascript? - Marcelo Galdino",
-        duration: "5min read",
-        price: "FREE"
-    },
-
-    {
-        link: "https://miro.medium.com/max/2560/0*nwlsfa0hEsSQihqw",
-        title: "Post sobre Javascript no medium | Afinal o que é Javascript? - Marcelo Galdino",
-        duration: "5min read",
-        price: "FREE"
     }
+
 ]
